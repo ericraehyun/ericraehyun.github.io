02eb2f6da0f62e74d85f9f80f6dff9af2c66926c
정승연 <nicole_chung@jeongseung-yeon-ui-MacBookAir.local>
2020-01-06 18:01:12 -0800

    jan 5

--- drawing/drawName.js
+++ drawing/drawName.js
@@ -1,20 +1,67 @@
 function drawName() {
-    drawA();
-    drawL();
     drawE();
-}
-function drawA() {
-    ctx.fillRect(2*UNIT,0,UNIT,UNIT);
-    ctx.fillRect(3*UNIT,0,UNIT,7*UNIT);
-    ctx.fillRect(1*UNIT,1*UNIT,1*UNIT,1*UNIT);
-    ctx.fillRect(0*UNIT,2*UNIT,1*UNIT,5*UNIT);
-    ctx.fillRect(1*UNIT,4*UNIT,2*UNIT,1*UNIT);
-}
-function drawL() {
-    ctx.fillRect(5*UNIT,0*UNIT,1*UNIT,7*UNIT);
-    ctx.fillRect(6*UNIT,6*UNIT,3*UNIT,1*UNIT);
+    drawR();
+    drawI();
+    drawC();
+    drawC2();
+    drawH();
+    drawU();
+    drawN();
+    drawG();
 }
 function drawE() {
-    ctx.fillRect(10*UNIT,0*UNIT,4*UNIT,UNIT);
-    ctx.fillRect(*UNIT,*UNIT,*UNIT,*UNIT);
+    ctx.fillRect(0*UNIT,0*UNIT,4*UNIT,1*UNIT);
+    ctx.fillRect(0*UNIT,1*UNIT,1*UNIT,6*UNIT);
+    ctx.fillRect(1*UNIT,3*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(1*UNIT,6*UNIT,3*UNIT,1*UNIT);
+}
+function drawR() {
+    ctx.fillRect(5*UNIT,0*UNIT,1*UNIT,7*UNIT);
+    ctx.fillRect(6*UNIT,0*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(8*UNIT,1*UNIT,1*UNIT,3*UNIT);
+    ctx.fillRect(6*UNIT,4*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(7*UNIT,5*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(8*UNIT,6*UNIT,1*UNIT,1*UNIT);
+}
+function drawI() {
+    ctx.fillRect(10*UNIT,0*UNIT,1*UNIT,7*UNIT);
+}
+function drawC() {
+    ctx.fillRect(12*UNIT,1*UNIT,1*UNIT,5*UNIT);
+    ctx.fillRect(13*UNIT,0*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(15*UNIT,1*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(13*UNIT,6*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(15*UNIT,5*UNIT,1*UNIT,1*UNIT);
+}
+function drawC2() {
+    ctx.fillRect(20*UNIT,0*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(19*UNIT,1*UNIT,1*UNIT,5*UNIT);
+    ctx.fillRect(20*UNIT,6*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(22*UNIT,1*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(22*UNIT,5*UNIT,1*UNIT,1*UNIT);
+}
+function drawH() {
+    ctx.fillRect(24*UNIT,0*UNIT,1*UNIT,7*UNIT);
+    ctx.fillRect(27*UNIT,0*UNIT,1*UNIT,7*UNIT);
+    ctx.fillRect(25*UNIT,3*UNIT,2*UNIT,1*UNIT);
+}
+function drawU() {
+    ctx.fillRect(29*UNIT,0*UNIT,1*UNIT,6*UNIT);
+    ctx.fillRect(30*UNIT,6*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(32*UNIT,0*UNIT,1*UNIT,6*UNIT);
+}
+function drawN() {
+    ctx.fillRect(34*UNIT,0*UNIT,1*UNIT,7*UNIT);
+    ctx.fillRect(35*UNIT,2*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(36*UNIT,3*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(37*UNIT,4*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(38*UNIT,0*UNIT,1*UNIT,7*UNIT);
+}
+function drawG() {
+    ctx.fillRect(40*UNIT,1*UNIT,1*UNIT,5*UNIT);
+    ctx.fillRect(41*UNIT,0*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(43*UNIT,1*UNIT,1*UNIT,1*UNIT);
+    ctx.fillRect(41*UNIT,6*UNIT,2*UNIT,1*UNIT);
+    ctx.fillRect(43*UNIT,4*UNIT,1*UNIT,2*UNIT);
+    ctx.fillRect(42*UNIT,4*UNIT,1*UNIT,1*UNIT);
 }
